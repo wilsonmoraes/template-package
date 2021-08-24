ab81c8f16ec590b737425d45b01252c6c994e8de
Wilson <wilsontads@gmail.com>
2021-08-24 12:14:56 -0300

    initial commit

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def validate_empty_fields(data, field):
+    if not data:
+        print(f"ERROR: '{data}' is not a valid {field}!")
+        sys.exit(1)
+
+
+validate_empty_fields('{{ cookiecutter.project_short_description }}', "project_short_description")
+validate_empty_fields('{{ cookiecutter.full_documentation_url }}', "full_documentation_url")
